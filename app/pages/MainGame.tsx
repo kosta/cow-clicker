f600de446109405191abe985c5b618f24f4df7ed
Kosta Welke <konstantin.welke@octomind.dev>
2025-10-08 10:14:35 +0200

    1 click per second

--- app/pages/MainGame.tsx
+++ app/pages/MainGame.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router'
 import cowWhiteSrc from '@/assets/cow-white.png'
 import fieldSrc from '@/assets/field.png'
@@ -6,9 +6,24 @@ import backgroundSkySrc from '@/assets/background-sky.png'
 import '@/pages/MainGame.css'
 import { MainRow } from '@/components/MainRow'
 
+const NEXT_CLICK_DELAY = 1000; // 1 second
+
 export const MainGame = () => {
   const [count, setCount] = useState(0)
+  const [preventClickUntil, setPreventClickUntil] = useState<number | null>(null)
   const navigate = useNavigate()
+  const handleClick = () => {
+    setPreventClickUntil(new Date(Date.now() + NEXT_CLICK_DELAY).getTime())
+    setCount((count) => count + 1)
+  }
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (preventClickUntil && Date.now() > preventClickUntil) {
+        setPreventClickUntil(null)
+      }
+    }, 50);
+    return () => clearInterval(intervalId)
+  }, [preventClickUntil])
 
   return (
     <MainRow backgroundImageSrc={backgroundSkySrc}>
@@ -25,9 +40,10 @@ export const MainGame = () => {
         <img src={fieldSrc} className="field" alt="the field that the cow stands on" aria-hidden="true" />
         <button
           type="button"
-          onClick={() => setCount((count) => count + 1)}
+          onClick={handleClick}
+          disabled={preventClickUntil !== null}
           aria-label="Click the cow"
-          className="cow-button hover-highlight unstyled-button"
+          className={`cow-button ${preventClickUntil === null ? 'hover-highlight' : ''} unstyled-button`}
         >
           <img src={cowWhiteSrc} className="cow" alt="" aria-hidden="true" />
         </button>
